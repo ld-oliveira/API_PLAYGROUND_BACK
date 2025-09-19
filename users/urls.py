7fe6306f23fce47985c2954459739fb88c72414f
ld-oliveira <ld-oliveira@uol.com>
2025-09-19 04:08:04 -0300

    urls

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_user, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('logout/', views.logout_user, name='logout'),
+    
 ]
