70de5f9812359a8a02b93a4e1b2401fa5327ed1e
ld-oliveira <ld-oliveira@uol.com>
2025-09-29 18:26:11 -0300

    urls

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from users.views import cadastro, login_user, logout_user, listar_usuarios, usuario_por_id, csrf_token
+from users.views import csrf_token,cadastro,login_user,logout_user,listar_usuarios,usuario_por_id
 
 urlpatterns = [
-    path("users/csrf/", csrf_token, name="csrf"),
-    path("users/cadastro/", cadastro, name="cadastro"),
-    path("users/login/", login_user, name="login"),
-    path("users/logout/", logout_user, name="logout"),
-    path("users/", listar_usuarios, name="listar_usuarios"),
-    path("users/<int:user_id>/", usuario_por_id, name="usuario_por_id"),
+    path("csrf/", csrf_token, name="csrf"),
+    path("cadastro/", cadastro, name="cadastro"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
+    path("", listar_usuarios, name="listar_usuarios"),
+    path("<int:user_id>/", usuario_por_id, name="usuario_por_id"),
 ]
