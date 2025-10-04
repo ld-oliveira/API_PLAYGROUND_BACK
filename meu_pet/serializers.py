d008f08f749f034e607f7594ae799d57047c771d
ld-oliveira <ld-oliveira@uol.com>
2025-10-04 02:08:46 -0300

    usuario

--- meu_pet/serializers.py
+++ meu_pet/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import Animal
 
 class AnimalSerializer(serializers.ModelSerializer):
+    usuario = serializers.SerializerMethodField()
+
     class Meta:
         model = Animal
         fields = ['id', 'nome', 'idade', 'foto', 'descricao', 'usuario', 'criado_em']
         read_only_fields = ['usuario', 'criado_em']
+
+    def get_usuario(self, obj):
+        if obj.usuario:
+            return obj.usuario.username
+        return None
