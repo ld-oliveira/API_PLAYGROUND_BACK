3e67d2bf922f02f4fe7fd49e7d3613f7b0f5cca4
ld-oliveira <ld-oliveira@uol.com>
2025-10-05 04:34:55 -0300

    models

--- meu_pet/models.py
+++ meu_pet/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary_storage.storage import MediaCloudinaryStorage  # 👈 IMPORTANTE
 
 class Animal(models.Model):
     nome = models.CharField(max_length=30)
     idade = models.IntegerField()
-    foto = models.ImageField(upload_to='fotos_animais')
+    foto = models.ImageField(storage=MediaCloudinaryStorage(), upload_to='fotos_animais')
     descricao = models.TextField()
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)  
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     criado_em = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"{self.nome} ({self.usuario.username})"
