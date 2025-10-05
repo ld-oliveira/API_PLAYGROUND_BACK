7f1a37b3ac9515df16d7c6ade06f2ac64e23f4b1
ld-oliveira <ld-oliveira@uol.com>
2025-10-05 02:04:29 -0300

    Cloud set

--- api_teste/settings.py
+++ api_teste/settings.py
@@ -28,6 +28,13 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
 }
 
+cloudinary.config( 
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+)
+
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = ''  # Cloudinary gerencia a URL final
 
