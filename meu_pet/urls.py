e40d3d1868060ad735905d6fa7fdb7eb5484870a
ld-oliveira <ld-oliveira@uol.com>
2025-10-26 21:29:56 -0300

    View edit

--- meu_pet/urls.py
+++ meu_pet/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AnimalListCreateView
+from .views import AnimalListCreateView, AnimalDetailView
 
 urlpatterns = [
     path('animais/', AnimalListCreateView.as_view(), name='animais-list-create'),
+    path('animais/<int:pk>/', AnimalDetailView.as_view(), name='animais-detail'),
 ]
--- meu_pet/views.py
+++ meu_pet/views.py
@@ -1,11 +1,24 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, parsers
 from .models import Animal
 from .serializers import AnimalSerializer
 
 class AnimalListCreateView(generics.ListCreateAPIView):
-    queryset = Animal.objects.all ()
+    queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #apenas quem está logado modifica
-    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
+
+class AnimalDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = AnimalSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+
+    def get_queryset(self):
+        from rest_framework.permissions import SAFE_METHODS
+        if self.request.method in SAFE_METHODS:
+            return Animal.objects.all()
+        if self.request.user.is_authenticated:
+            return Animal.objects.filter(usuario=self.request.user)
+        return Animal.objects.none()
